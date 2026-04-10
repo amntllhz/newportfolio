739510067c0938124c005526c73782a4e48d5366
amntllhz <amanatullah1904@gmail.com>
2026-04-10 19:58:35 +0700

    style: education scroll bug fix

--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -6,8 +6,8 @@ import Technical from "@/organism/Technical"
 const Education = () => {
     return (
         <>
-            <div className="flex h-screen justify-center items-center w-full">
-                <ScrollArea className="lg:px-10 xs:px-0 lg:h-100 xs:h-screen lg:mask-[linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)] mask-[linear-gradient(to_bottom,black_0%,black_90%,transparent)]">
+            <div className="flex lg:h-screen lg:justify-center lg:items-center w-full">
+                <ScrollArea className="lg:px-10 xs:px-0 lg:h-100 xs:h-dvh lg:mask-[linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)] mask-[linear-gradient(to_bottom,black_0%,black_90%,transparent)]">
                     <div className="flex flex-col justify-center items-center">
                         <Technical></Technical>
                         <Experience></Experience>
