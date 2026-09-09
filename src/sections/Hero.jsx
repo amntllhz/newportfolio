981813b8bd846678b8fbd07aab789d78ad6b2607
amntllhz <amanatullah1904@gmail.com>
2026-09-09 12:05:55 +0700

    data: update data

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,7 +8,7 @@ const containerVariants = {
     opacity: 1,
     transition: {
       // Memberikan jeda 0.2 detik antar elemen anak
-      staggerChildren: 0.1, 
+      staggerChildren: 0.1,
       delayChildren: 0.1,
     },
   },
@@ -16,38 +16,38 @@ const containerVariants = {
 
 const itemVariants = {
   hidden: { opacity: 0, y: 30 },
-  visible: { 
-    opacity: 1, 
-    y: 0, 
-    transition: { type: "spring", stiffness: 100 } 
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 100 }
   },
 };
 
 const Hero = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <>
-            <motion.div initial="hidden" animate="visible" variants={containerVariants} className="w-full h-dvh flex gap-6 items-center justify-center lg:flex-row xs:flex-col">                
-                <Card variants={itemVariants}></Card> 
-                <div className="flex flex-col gap-2 lg:max-w-lg xs:max-w-9/10">
-                        <motion.h1 variants={itemVariants} className="font-main font-semibold lg:text-xl xs:text-md text-gray-900 lg:text-left xs:text-center dark:text-white">
-                          {t("hero.greeting")}
-                        </motion.h1> 
-                        <motion.p variants={itemVariants} className="font-main text-xs text-gray-400 text-justify leading-normal dark:text-neutral-400">
-                            {t("hero.p1_start")}
-                            <span className=" font-semibold text-gray-500/80 dark:text-neutral-100"> Front-end Web Developer, Graphic Designer, </span> 
-                            {t("hero.p1_middle")}
-                            <span className="font-semibold text-gray-500/80 dark:text-neutral-100"> UI Designer </span>
-                            {t("hero.p1_end")}
-                        </motion.p>
-                        <motion.p variants={itemVariants} className="font-main text-xs text-gray-400 text-justify leading-normal dark:text-neutral-400">
-                            {t("hero.p2")}
-                        </motion.p>                    
-                </div>
-            </motion.div>
-        </>
-    )
+  return (
+    <>
+      <motion.div initial="hidden" animate="visible" variants={containerVariants} className="w-full h-dvh flex gap-6 items-center justify-center lg:flex-row xs:flex-col">
+        <Card variants={itemVariants}></Card>
+        <div className="flex flex-col gap-2 lg:max-w-lg xs:max-w-9/10">
+          <motion.h1 variants={itemVariants} className="font-main font-semibold lg:text-xl xs:text-md text-gray-900 lg:text-left xs:text-center dark:text-white">
+            {t("hero.greeting")}
+          </motion.h1>
+          <motion.p variants={itemVariants} className="font-main text-xs text-gray-400 text-justify leading-normal dark:text-neutral-400">
+            {t("hero.p1_start")}
+            <span className=" font-semibold text-gray-500/80 dark:text-neutral-100"> Web Developer </span>
+            {t("hero.p1_middle")}
+            <span className="font-semibold text-gray-500/80 dark:text-neutral-100"> Graphic Designer </span>
+            {t("hero.p1_end")}
+          </motion.p>
+          <motion.p variants={itemVariants} className="font-main text-xs text-gray-400 text-justify leading-normal dark:text-neutral-400">
+            {t("hero.p2")}
+          </motion.p>
+        </div>
+      </motion.div>
+    </>
+  )
 }
 
 export default Hero
